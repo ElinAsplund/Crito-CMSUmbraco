f36b396a4ad1e2d1a99f31bdb3eb884703dacde7
Elin Asplund <elin.asplund91@gmail.com>
2023-09-11 16:49:48 +0200

    added JS and new partials

--- wwwroot/scripts/script.js
+++ wwwroot/scripts/script.js
@@ -1,2 +1,13 @@
+// Active navLink.
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('a.page-route');
 
-                        
+if (activePage === "/") {
+    navLinks[0].classList.add('active')
+} else {
+    navLinks.forEach(link => {
+        if (link.href.includes(`${activePage}`)) {
+            link.classList.add('active');
+        }
+    });
+}
